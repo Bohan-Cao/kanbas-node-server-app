55708d4a361a94f57f93fa70eb877dc4ba464b3a
Bohan-Cao <cao.boh@northeastern.edu>
2024-04-18 10:49:59 +0800

    "a6 Mongo su23"

--- App.js
+++ App.js
@@ -1,17 +1,46 @@
+import "dotenv/config";
 import AssignmentRoutes from "./Kanbas/assignments/routes.js";
 import CourseRoutes from "./Kanbas/courses/routes.js";
 import Hello from "./Hello.js";
 import Lab5 from "./Lab5.js";
 import ModuleRoutes from "./Kanbas/modules/routes.js";
+import UserRoutes from "./Kanbas/Users/routes.js";
 import cors from "cors";
 import express from "express";
+import mongoose from "mongoose";
+import session from "express-session";
+
+const CONNECTION_STRING =
+    process.env.DB_CONNECTION_STRING || "mongodb://127.0.0.1:27017/kanbas";
+mongoose.connect(CONNECTION_STRING);
 
 const app = express();
-app.use(cors());
+app.use(
+    cors({
+        credential: true,
+        origin: process.env.FRONTEND_URL,
+    })
+);
+const sessionOptions = {
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+};
+if (process.env.NODE_ENV !== "development") {
+    sessionOptions.proxy = true;
+    sessionOptions.cookie = {
+        sameSite: "none",
+        secure: true,
+        domain: process.env.HTTP_SERVER_DOMAIN,
+    };
+}
+app.use(session(sessionOptions));
 app.use(express.json());
+const port = process.env.PORT || 4000;
 Hello(app);
 ModuleRoutes(app);
-AssignmentRoutes(app)
+AssignmentRoutes(app);
 CourseRoutes(app);
 Lab5(app);
-app.listen(4000);
+UserRoutes(app);
+app.listen(port);
