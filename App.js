c14627c7aec2d559bd3f9731299f4b333102f8a9
Bohan-Cao <cao.boh@northeastern.edu>
2024-04-19 01:39:34 +0800

    "edit App.js"

--- App.js
+++ App.js
@@ -32,9 +32,12 @@ function logErrors(err, req, res, next) {
     next(err);
 }
 
-function logErrors(err, req, res, next) {
-    console.error(err.stack);
-    next(err);
+function clientErrorHandler(err, req, res, next) {
+    if (req.xhr) {
+        res.status(500).send({ error: 'Something failed!' });
+    } else {
+        next(err);
+    }
 }
 
 function errorHandler(err, req, res, next) {
